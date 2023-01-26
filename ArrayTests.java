6106b3f3f8e1e62120418f3ec0ea3a8155bdc674
itsesO3O <122569641+itsesO3O@users.noreply.github.com>
2023-01-26 12:39:32 -0800

    changes were made

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -14,5 +17,18 @@ public class ArrayTests {
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReverseIP2(){
+    int[] input2 = {3, 4, 21, 2, 10};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[] {10, 2, 21, 4, 3}, input2 );
+  }
+
+  @Test 
+  public void testReversed2(){
+    int[] input3 = {3, 4, 21, 2, 10};
+    assertArrayEquals(new int[]{10, 2, 21, 4, 3}, ArrayExamples.reversed(input3));
   }
 }
